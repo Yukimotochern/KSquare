b86aac731b4c0619d9ae127a2c54d84e36769109
Yukimotochern <shtomcom@gmail.com>
2018-11-19 18:13:56 +0800

    Setting up

--- KSquare/settings.py
+++ KSquare/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'KSquare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ks',
+        'USER': 'yukimoto',
+        'PASSWORD': 'a130153113',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
